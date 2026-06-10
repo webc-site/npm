4b3aee823ecc2fee32c4c8f7c537b232cfaf01ee
x-at-01 <x-at-01@googlegroups.com>
2026-06-10 20:45:26 +0800

    chore: implement publish.js and update cersei_rs workflow

--- _/rs/github_action/pkg_name.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bun
-import read from "@3-/read";
-import path from "path";
-
-const dir = process.argv[2] || ".",
-  pkg = JSON.parse(read(path.join(dir, "package.json")));
-console.log(pkg.name);
