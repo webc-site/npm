77768a87f3d0fbc62dd8feec21bc69de734aceda
x-at-01 <x-at-01@googlegroups.com>
2026-06-10 20:26:54 +0800

    chore: remove templates and make cersei_rs workflow independent

--- cersei_rs/tests/setup.js
+++ cersei_rs/tests/setup.js
@@ -1,17 +1,12 @@
+import { env } from "node:process";
 import { test as bun_test } from "bun:test";
 import path from "node:path";
+import openai_conf from "../../conf/OPENAI.js";
 
-let base_url, api_key, model;
-if (process.env.OPENAI_LI) {
-  [base_url, api_key, model] = JSON.parse(process.env.OPENAI_LI);
-} else {
-  const openai_conf = (await import("../../conf/OPENAI.js")).default;
-  [base_url, api_key, model] = openai_conf;
-}
-
-const __dirname = import.meta.dirname,
+const [base_url, api_key, model] = openai_conf,
+  __dirname = import.meta.dirname,
   gen_dir = path.join(__dirname, "gen"),
-  is_test_runner = process.env.NODE_ENV === "test",
+  is_test_runner = env.NODE_ENV === "test",
   test = is_test_runner
     ? bun_test
     : async (name, fn) => {
