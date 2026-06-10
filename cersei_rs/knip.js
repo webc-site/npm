d31c12a3a8dd165d03abab197bfeada9a536087d
x-at-01 <x-at-01@googlegroups.com>
2026-06-10 20:59:47 +0800

    chore: add set -ex to build test step and register ops/test.js in knip configuration

--- cersei_rs/knip.js
+++ cersei_rs/knip.js
@@ -1,4 +1,11 @@
 export default {
-  entry: ["tests/*.js", "./build.js", "./dist.js", "./ops/npmInit.js", "./ops/prepublish.js"],
+  entry: [
+    "tests/*.js",
+    "./build.js",
+    "./dist.js",
+    "./ops/npmInit.js",
+    "./ops/prepublish.js",
+    "./ops/test.js",
+  ],
   ignore: ["src/index.js", "src/index.d.ts"],
 };
