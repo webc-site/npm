be5c18ed1d2f464a5a71de547122fa6ca8a01939
x-at-01 <x-at-01@googlegroups.com>
2026-06-15 12:21:05 +0800

    init rank

---
+++ rank/tests/main.rs
@@ -0,0 +1,13 @@
+use aok::{OK, Void};
+use log::info;
+
+#[static_init::constructor(0)]
+extern "C" fn _log_init() {
+  log_init::init();
+}
+
+#[test]
+fn test() -> Void {
+  info!("> test {}", 123456);
+  OK
+}
