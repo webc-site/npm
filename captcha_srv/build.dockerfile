3bf1ef4dfb6b6e66120ddfcceac50ff574d5024a
x-at-01 <x-at-01@googlegroups.com>
2026-08-04 17:48:01 +0800

    .

--- captcha_srv/build.dockerfile
+++ captcha_srv/build.dockerfile
@@ -1,6 +1,9 @@
-FROM --platform=linux/amd64 rust:latest
+ARG PLATFORM=linux/amd64
+FROM --platform=${PLATFORM} rust:latest
 
-RUN rustup target add x86_64-unknown-linux-musl \
+ARG TARGET=x86_64-unknown-linux-musl
+
+RUN rustup target add ${TARGET} \
  && apt-get update \
  && apt-get install -y musl-tools musl-dev \
  && rm -rf /var/lib/apt/lists/*
--- captcha_srv/build.sh
+++ captcha_srv/build.sh
@@ -5,18 +5,40 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -x
 
-TARGET="x86_64-unknown-linux-musl"
+CPU="${CPU:-amd64}"
+
+case "$CPU" in
+  amd64|x86_64)
+    CPU="amd64"
+    TARGET="x86_64-unknown-linux-musl"
+    ;;
+  arm64|aarch64)
+    CPU="arm64"
+    TARGET="aarch64-unknown-linux-musl"
+    ;;
+  *)
+    echo "Unsupported CPU: $CPU"
+    exit 1
+    ;;
+esac
+
+PLATFORM="linux/$CPU"
 OUT_DIR="bin/$TARGET"
 mkdir -p "$OUT_DIR"
 
 IMAGE="captcha_srv_builder:latest"
-docker build --platform linux/amd64 -t "$IMAGE" -f build.dockerfile .
+docker build \
+  --platform "$PLATFORM" \
+  --build-arg PLATFORM="$PLATFORM" \
+  --build-arg TARGET="$TARGET" \
+  -t "$IMAGE" \
+  -f build.dockerfile .
 
 ROOT_DIR=$(realpath $DIR/../../..)
 CACHE_DIR="/tmp/captcha_srv_build"
 mkdir -p "$CACHE_DIR/cargo" "$CACHE_DIR/target"
 
-docker run --platform linux/amd64 --rm \
+docker run --platform "$PLATFORM" --rm \
   -v "$ROOT_DIR:/workspace" \
   -v "$CACHE_DIR/cargo:/usr/local/cargo/registry" \
   -v "$CACHE_DIR/target:/tmp/target" \
