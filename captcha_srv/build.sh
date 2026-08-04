6e7348bfb5d49ed088cc8e3389aee00692aadb19
x-at-01 <x-at-01@googlegroups.com>
2026-08-04 17:53:19 +0800

    .

--- captcha_srv/build.sh
+++ captcha_srv/build.sh
@@ -5,47 +5,52 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -x
 
-CPU="${CPU:-amd64}"
-
-case "$CPU" in
-  amd64|x86_64)
-    CPU="amd64"
-    ARCH="x86_64"
-    ;;
-  arm64|aarch64)
-    CPU="arm64"
-    ARCH="aarch64"
-    ;;
-  *)
-    echo "Unsupported CPU: $CPU"
-    exit 1
-    ;;
-esac
-
-TARGET="${ARCH}-unknown-linux-musl"
-PLATFORM="linux/$CPU"
-OUT_DIR="bin/$TARGET"
-mkdir -p "$OUT_DIR"
-
-IMAGE="captcha_srv_builder:latest"
-docker build \
-  --platform "$PLATFORM" \
-  --build-arg PLATFORM="$PLATFORM" \
-  --build-arg TARGET="$TARGET" \
-  -t "$IMAGE" \
-  -f build.dockerfile .
-
 ROOT_DIR=$(realpath $DIR/../../..)
 CACHE_DIR="/tmp/captcha_srv_build"
 mkdir -p "$CACHE_DIR/cargo" "$CACHE_DIR/target"
 
-docker run --platform "$PLATFORM" --rm \
-  -v "$ROOT_DIR:/workspace" \
-  -v "$CACHE_DIR/cargo:/usr/local/cargo/registry" \
-  -v "$CACHE_DIR/target:/tmp/target" \
-  -e CARGO_TARGET_DIR=/tmp/target \
-  -w /workspace/git/npm/captcha_srv \
-  "$IMAGE" \
-  cargo build --release --target "$TARGET"
+run() {
+  local CPU=$1
+  case "$CPU" in
+    amd64|x86_64)
+      CPU="amd64"
+      ARCH="x86_64"
+      ;;
+    arm64|aarch64)
+      CPU="arm64"
+      ARCH="aarch64"
+      ;;
+    *)
+      echo "Unsupported CPU: $CPU"
+      exit 1
+      ;;
+  esac
 
-cp "$CACHE_DIR/target/$TARGET/release/captcha_srv" "$OUT_DIR/"
+  local TARGET="${ARCH}-unknown-linux-musl"
+  local PLATFORM="linux/$CPU"
+  local OUT_DIR="bin/$TARGET"
+  mkdir -p "$OUT_DIR"
+
+  local IMAGE="captcha_srv_builder:$CPU"
+  docker build \
+    --platform "$PLATFORM" \
+    --build-arg PLATFORM="$PLATFORM" \
+    --build-arg TARGET="$TARGET" \
+    -t "$IMAGE" \
+    -f build.dockerfile .
+
+  docker run --platform "$PLATFORM" --rm \
+    -v "$ROOT_DIR:/workspace" \
+    -v "$CACHE_DIR/cargo:/usr/local/cargo/registry" \
+    -v "$CACHE_DIR/target:/tmp/target" \
+    -e CARGO_TARGET_DIR=/tmp/target \
+    -w /workspace/git/npm/captcha_srv \
+    "$IMAGE" \
+    cargo build --release --target "$TARGET"
+
+  cp "$CACHE_DIR/target/$TARGET/release/captcha_srv" "$OUT_DIR/"
+}
+
+run amd64
+run arm64
+
