2f0fa642ce37dabf5724168ac906540506d7098b
x-at-01 <x-at-01@googlegroups.com>
2026-08-06 16:36:00 +0800

    .

--- smtp_srv/src/api/auth.rs
+++ smtp_srv/src/api/auth.rs
@@ -4,6 +4,7 @@ use axum::{
   middleware::Next,
   response::Response,
 };
+use base64::{engine::general_purpose::STANDARD, Engine};
 
 genv::s!(SMTP_API_USER: String);
 genv::s!(SMTP_API_PASSWORD: String);
@@ -16,7 +17,7 @@ pub async fn auth(
   if let Some(auth_header) = headers.get(AUTHORIZATION) {
     if let Ok(auth_str) = auth_header.to_str() {
       if let Some(strip) = auth_str.strip_prefix("Basic ") {
-        if let Ok(decoded) = intbin::b64_decode(strip) {
+        if let Ok(decoded) = STANDARD.decode(strip.trim()) {
           if let Ok(credentials) = String::from_utf8(decoded) {
             if let Some((user, pass)) = credentials.split_once(':') {
               if user == *SMTP_API_USER && pass == *SMTP_API_PASSWORD {
