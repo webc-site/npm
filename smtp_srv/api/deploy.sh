25f5b6a713f9ddd34dcbf511b4fab1acf309fd4d
x-at-01 <x-at-01@googlegroups.com>
2026-08-06 16:23:00 +0800

    .

--- smtp_srv/api/deploy.sh
+++ smtp_srv/api/deploy.sh
@@ -5,8 +5,9 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -x
 
-ENV=../../../conf/prod/smtp/api.env
-[ -f "$ENV" ] && bunx wrangler secret bulk "$ENV"
+CONF_DIR=../../../conf/prod/smtp
+cat "$CONF_DIR/conf.env" "$CONF_DIR/default.env" "$CONF_DIR/api.env" 2>/dev/null | bunx wrangler secret bulk
 
 bun run deploy
+
 
